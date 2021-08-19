88e5b348a75a8afa7a38b02dc13d2254e8d689cf
who000238 <who000238@gmail.com>
2021-08-19 11:37:32 +0800

    【重構】AccountingList WebAPI - 調整成強型別方法；加入 string 轉 GUID 的擴充方法

---
+++ WebformMiniSample/AccountingNote/Extensions/StringExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AccountingNote.Extensions
+{
+    public static class StringExtension
+    {
+        public static Guid ToGuid(this string guidText)
+        {
+            if(Guid.TryParse(guidText,out Guid tempGuid))
+            {
+                return tempGuid;
+            }
+            else
+            {
+                return Guid.Empty;
+            }
+        }
+    }
+}
--- WebformMiniSample/AccountingNote/Handlers/AccountingNoteList.ashx.cs
+++ WebformMiniSample/AccountingNote/Handlers/AccountingNoteList.ashx.cs
@@ -1,5 +1,7 @@
 ﻿using AccountingNote.DBSource;
+using AccountingNote.Extensions;
 using AccountingNote.Model;
+using AccountingNote.ORM.DBModels;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -35,22 +37,35 @@ namespace AccountingNote.Handlers
             }
 
             string userID = dr["ID"].ToString();
-            DataTable dataTable = AccountingManager.GetAccountingList(userID);
+            Guid userGUID = userID.ToGuid();
+            List<Accounting> sourceList = AccountingManager.GetAccountingList(userGUID);
 
-            List<AccountingNoteViewModel> list = new List<AccountingNoteViewModel>();
-            foreach (DataRow drAccounting in dataTable.Rows)
-            {
-                AccountingNoteViewModel model = new AccountingNoteViewModel()
-                {
-                    ID = drAccounting["ID"].ToString(),
-                    Caption = drAccounting["Caption"].ToString(),
-                    Amount = drAccounting.Field<int>("Amount"),
-                    ActType = (drAccounting.Field<int>("ActType") == 0) ? "支出" : "收入",
-                    CreateDate = drAccounting.Field<DateTime>("CreateDate").ToString("yyyy-MM-dd")
-                };
+            //DataTable dataTable = AccountingManager.GetAccountingList(userID);
 
-                list.Add(model); 
-            }
+            //List<AccountingNoteViewModel> list = new List<AccountingNoteViewModel>();
+            //foreach (DataRow drAccounting in dataTable.Rows)
+            //{
+            //    AccountingNoteViewModel model = new AccountingNoteViewModel()
+            //    {
+            //        ID = drAccounting["ID"].ToString(),
+            //        Caption = drAccounting["Caption"].ToString(),
+            //        Amount = drAccounting.Field<int>("Amount"),
+            //        ActType = (drAccounting.Field<int>("ActType") == 0) ? "支出" : "收入",
+            //        CreateDate = drAccounting.Field<DateTime>("CreateDate").ToString("yyyy-MM-dd")
+            //    };
+
+            //    list.Add(model); 
+            //}
+            List<AccountingNoteViewModel> list =
+                    sourceList.Select(obj => new AccountingNoteViewModel()
+                    {
+                        ID = obj.ID.ToString(),
+                        Caption = obj.Caption,
+                        Amount = obj.Amount,
+                        ActType =
+                                (obj.ActType == 0) ? "支出" : "收入",
+                        CreateDate = obj.CreateDate.ToString("yyyy-MM-dd")
+                    }).ToList();
 
             string jsonText = Newtonsoft.Json.JsonConvert.SerializeObject(list);
 
